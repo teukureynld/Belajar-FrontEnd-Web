aeaced3d4ab2622436e8b3c97ba1eec0bc91e146
Mystereyn <teukureynaldi106@gmail.com>
2022-08-20 19:07:55 +0700

    add file JAVASCRIPT/arrayLoop.js

---
+++ JAVASCRIPT/arrayLoop.js
@@ -0,0 +1,13 @@
+let for_index = [0,1,2,3,4,5,6,7,8,9,10];
+
+let for_data = ["volvo", "mit" , "max"];
+
+// ini sama tapi mewakili index
+for(let index in for_index) {
+    console.log(index);
+}
+
+// ini untuk jika mau langsung cetak array semuanya
+for(let Data of for_data) {
+    console.log(Data);
+}
