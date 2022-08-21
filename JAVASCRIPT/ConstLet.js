68f4f70e8e02aff145596e962216014b290923c7
Mystereyn <teukureynaldi106@gmail.com>
2022-08-21 20:44:34 +0700

    add JAVASCRIPT/DOM.JS && ConstLet.js

---
+++ JAVASCRIPT/ConstLet.js
@@ -0,0 +1,13 @@
+function getName() {
+    //let = variable yang bisa diubah datanya
+    let nama = "Teuku Reynaldi"
+    nama = "Putra";
+
+    console.log(nama);
+    // jika konstanta, maka data tidak bisa diubah
+    const nama2 = "Irwansyah";
+    nama2 = "Reynaldi"
+    console.log(nama2);
+}
+
+getName();
