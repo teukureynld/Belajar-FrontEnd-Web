7115a503e571f2e208dba3630714dfe82a6c8ed3
Mystereyn <teukureynaldi106@gmail.com>
2022-09-11 21:47:14 +0700

    Modified Javasript/TypeFunction

--- JAVASCRIPT/TypeFunction.js
+++ JAVASCRIPT/TypeFunction.js
@@ -4,9 +4,22 @@ const showName  = function (name) {
 }
 console.log(showName('Teuku'));
 
+
 // ! Kalo ini adalah contoh dari arrow function
 const tampilNama =  (nama) => {
     return `Halo selamat malam, ${nama}`;
 }; 
-
 console.log(tampilNama('Reynaldi'));
+
+console.log();
+
+// ! Ini adalah function anonymous
+// ! Contoh untuk looping
+const anonymousFunction = function (i) {
+    for(i=0; i <= 25; i++) {
+        // console.log(i);
+    };
+    return `Contoh untuk panggil parameter , ${i};`
+}
+
+anonymousFunction();
