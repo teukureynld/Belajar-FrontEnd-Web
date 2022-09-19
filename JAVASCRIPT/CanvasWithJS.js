240c645766d217f2bfa4d1e57b2afff1c5319a85
Mystereyn <teukureynaldi106@gmail.com>
2022-09-19 10:27:26 +0700

    Modified Canvas File

---
+++ JAVASCRIPT/CanvasWithJS.js
@@ -0,0 +1,9 @@
+// // get element by id canvas
+// var canvas = document.getElementById("canvas");
+
+// // Canvas get context
+// var ctx = canvas.getContext("2d");
+
+// // Method untuk mengisi warna persegi panjang 
+// ctx.fillStyle = "#FF0000";
+// ctx.fillRect(0,0,150,75);
