cf8b3fdad8d4ae24ef97e10a387c91c0c1358e3f
Mystereyn <teukureynaldi106@gmail.com>
2022-09-26 23:59:57 +0700

    ngubahin file constOrang.js

--- JAVASCRIPT/constOrang.js
+++ JAVASCRIPT/constOrang.js
@@ -1,6 +1,30 @@
+// function accessModul () {
+//     requireModul()
+// }
+// function requireModul() {
+//     const console = require('console')
+// }
+
+
 const people = [
     ["Teuku" , "Reynaldi"],
     ["Putra" , "Irwansyah"],
 ];
 
 console.table(people);
+
+function listPeople() {
+   const listPeople = [
+    {
+        Nama_Depan : "Teuku Reynaldi",
+        Nama_Belakang: "Putra Irwansyah"
+    },
+    {
+        Nama_Depan : "Mamanx Garok",
+        Nama_Belakang : "Mamanx Kesbor"
+    }
+];
+   console.table(listPeople);
+};
+
+listPeople();
